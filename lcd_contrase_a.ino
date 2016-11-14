4d6a4f724b839a5f615d321a388474caabb3f81b
Yadi9218 <yafloresf@utn.edu.ec>
2016-11-14 05:53:40 -0500

    Add files via upload

---
+++ lcd_contrase_a.ino
@@ -0,0 +1,39 @@
+/*
+ * Nombre: Yadira Flores
+ * Tarea programa mensaje
+ */
+
+#include <LiquidCrystal.h>  //libreria necesaria para utilizar LCD
+
+LiquidCrystal lcd (12,11,5,4,3,2);
+
+String m_1 = "Contraseña";
+String m_2 = "Contraseña Aceptada";
+String m_3 = "Contraseña Denegada";
+String contr;
+
+void setup() {
+    Serial.begin(9600);
+    lcd.begin(16,2);
+
+}
+
+void loop() {
+  if (Serial.available()>0){     //dato por leer
+    contr=Serial.readString();   //Lee dato ingresado por teclado
+    Serial.println(contr);       //Imprime en terminal mensaje
+    lcd.clear();
+    if(contr=="9218A"){         //Compara el texto ingresado con la contraseña establecida
+      lcd.setCursor(0, 0);      //Ubica cursor lcd posicion 0,0
+      lcd.print(m_2);           //Imprime m_2
+      lcd.setCursor(0, 1);      //Ubica cursor lcd posicion 0,1
+      lcd.print(m_1);           //Imprime m_1
+    }
+    else{                       //en caso de contraseña incorrecta imprime
+      lcd.setCursor(0, 0);       //Ubica cursor lcd posicion 0,0
+      lcd.print(m_3);           //Imprime m_3
+      lcd.setCursor(0, 1);       //Ubica cursor lcd posicion 0,1
+      lcd.print(m_1);            //Imprime m_1
+    }
+  }  
+ }
---
+++ lcd_mensaje.ino
@@ -0,0 +1,34 @@
+/*
+ * Nombre: Yadira Flores
+ * Tarea programa contraseña
+ */
+ 
+#include <LiquidCrystal.h>           //libreria necesaria para utilizar LCD
+LiquidCrystal lcd (12,11,5,4,3,2);
+char mensaje [4];
+String m_ent;
+int cont=0; 
+
+void setup() {
+  Serial.begin(9600);
+  lcd.begin(16,2);
+  
+}
+
+void loop() {
+  if (Serial.available()>0){
+    m_ent=Serial.readString();                    // Lee mensaje que se ingresado mediante teclado
+    m_ent.toCharArray(mensaje,m_ent.length()+1);  // Convierte el mensaje ingresado en un vector
+    Serial.println(m_ent);                        // Imprime en terminal el mensaje
+  }  
+    lcd.clear();                                  // Borra la informacion de LCD
+    for(;cont<m_ent.length()+1;cont++){           // Contador desde cero hasta el numero de caracteres que tiene la palabra
+      lcd.setCursor(cont, 0);                     // Ubica el cursor en la posicion 0,0
+      lcd.print(mensaje[cont]);                   // Imprime el mensaje ingrasado por teclado
+      delay(300);
+    }    
+    cont=0;                                       //Reinicia el contador
+    
+}
+
+  
